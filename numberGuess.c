b8fc67804daa4ff18b5b283f36f82f878df4ccec
tommym-r <tommymro@gmail.com>
2026-01-13 22:14:30 +0000

    added more to numberGuess.c

--- numberGuess.c
+++ numberGuess.c
@@ -4,5 +4,17 @@
 int main(){
    char userName[20];
    int guess = -999
-   int answer = 
+   int answer
+   int turns = 0
+   int keepGoing = -1
+
+   srand(time(NULL));
+   answer = rand();
+   answer = answer % 100
+
+   printf("What is your name? \n ");
+   scanf("%s", userName);
+   printf("Hello, %s \n", userName);
+
+
 } //end main
